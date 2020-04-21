28cf9a1f21f0e819939195d111031f3ac278bd6a
leonel bustamante <49352240+leonelBusta@users.noreply.github.com>
2020-04-21 03:05:03 -0700

    Condiciones con operaciones logicas

---
+++ condiciones.js
@@ -0,0 +1,25 @@
+let num = 0;
+/**Cindicional simple */
+if (num > 0) {
+    console.log(`${num} es mayor que  0`);
+    console.log(`${num} es mayor que  0`);
+
+}
+/**Condicional compuesto */
+if (num > 0) {
+    console.log(`${num} es mayor que 0`);
+
+} else {
+    console.log(`${num} es menor que 0`);
+}
+
+/**Condicional multiple */
+if (num > 0) {
+    console.log(`${num} es mayor que 0`);
+
+} else if (num < 0) {
+    console.log(`${num} es menor que 0`);
+
+} else {
+    console.log(`${num} es igual a 0`);
+}
---
+++ operadoresLogicosCondicional.js
@@ -0,0 +1,10 @@
+let num1 = 1;
+let num2 = 5;
+
+/** && and  */
+/** || O */
+
+if (num1 > 0 && num2 > 0) {
+    console.log(`${num1} o ${num2} son mayores que 0`);
+
+}
